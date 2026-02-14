8f518c42c716fd5098a8c5baa02c804995bd8290
Devayani <jdevayani279@gmail.com>
2026-02-14 22:50:21 +0530

    URLrouting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Money Laundering Backend Running"}
+@app.post("/user/{username}/photo/{photoId}")
+def root(username:str,photoId:int,size:int,type:str="jpeg"):
+    
+    return {f"message":f"The user name is {username} , the id is {photoId}, the {size}, and {type}"}
