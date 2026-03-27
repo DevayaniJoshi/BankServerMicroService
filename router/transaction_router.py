8a4729d775960b501e4d69b909f0da2f39dfaeae
Devayani <jdevayani279@gmail.com>
2026-03-27 23:51:29 +0530

    step 1,2,3 intiatialized

--- router/transaction_router.py
+++ router/transaction_router.py
@@ -6,12 +6,10 @@ from models.transaction import Transaction
 from dto.transaction_create_dto import TransactionCreateDTO
 from dto.transaction_response_dto import TransactionResponseDTO
 from dto.transaction_update_dto import TransactionUpdateDTO
-from fastapi import HTTPException
 from uuid import UUID
 from config.kafka_config import get_kafka_producer
 from config.settings import ENABLE_KAFKA
-from fastapi import HTTPException
-
+from models.account import Account
 
 router = APIRouter(
     prefix="/api/transactions",
@@ -21,9 +19,38 @@ router = APIRouter(
 # CREATE
 @router.post("/", response_model=TransactionResponseDTO)
 def create_transaction(transaction: TransactionCreateDTO, db: Session = Depends(get_db)):
-    try:
 
+    # 🔥 STEP 1: Account Validation
+    account = db.query(Account).filter(
+        Account.accountId == transaction.fromAccount
+    ).first()
+
+    if not account:
+        raise HTTPException(status_code=400, detail="Sender account does not exist")
+
+    # 🔥 STEP 2: Filter Check
+    from models.account_filter import AccountFilter
+
+    filter_entry = db.query(AccountFilter).filter(
+        AccountFilter.accountId == transaction.fromAccount
+    ).first()
+
+    # 🔥 STEP 3: Status Decision
+    if filter_entry:
+        if filter_entry.blockingLevel.upper() == "BLACK":
+            status = "BLOCKED"
+        elif filter_entry.blockingLevel.upper() == "GREY":
+            status = "REVIEW"
+        else:
+            status = "SUCCESS"
+    else:
+        status = "SUCCESS"
+
+    try:
         new_transaction = Transaction(**transaction.model_dump())
+
+        # 🔥 ADD STATUS
+        new_transaction.status = status
 
         print(new_transaction)
 
@@ -33,16 +60,20 @@ def create_transaction(transaction: TransactionCreateDTO, db: Session = Depends(
 
         if ENABLE_KAFKA:
             kafka = get_kafka_producer()
-            kafka.send(KAFKA_TRANSACTION_TOPIC,TransactionResponseDTO.model_validate(new_transaction).model_dump(mode="json"))
+            kafka.send(
+                KAFKA_TRANSACTION_TOPIC,
+                TransactionResponseDTO.model_validate(new_transaction).model_dump(mode="json")
+            )
 
         return new_transaction
-        
+
     except Exception as e:
-        print("ERROR: ",e)
+        print("ERROR: ", e)
         raise HTTPException(
             status_code=500,
-            detail=e.__cause__
+            detail=str(e)
         )
+
 
 # GET ALL
 @router.get("/", response_model=list[TransactionResponseDTO])
@@ -53,9 +84,13 @@ def get_all_transactions(db: Session = Depends(get_db)):
 # GET BY ID
 @router.get("/{transactionId}", response_model=TransactionResponseDTO)
 def get_transaction(transactionId: UUID, db: Session = Depends(get_db)):
-    transaction = db.query(Transaction).filter(Transaction.transactionId == transactionId).first()
+    transaction = db.query(Transaction).filter(
+        Transaction.transactionId == transactionId
+    ).first()
+
     if not transaction:
         raise HTTPException(status_code=404, detail="Transaction not found")
+
     return transaction
 
 
@@ -81,13 +116,120 @@ def update_transaction(
 
     return transaction
 
+
 # DELETE
 @router.delete("/{transactionId}")
 def delete_transaction(transactionId: UUID, db: Session = Depends(get_db)):
-    transaction = db.query(Transaction).filter(Transaction.transactionId == transactionId).first()
+    transaction = db.query(Transaction).filter(
+        Transaction.transactionId == transactionId
+    ).first()
+
     if not transaction:
         raise HTTPException(status_code=404, detail="Transaction not found")
 
     db.delete(transaction)
     db.commit()
+
     return {"message": "Transaction deleted successfully"}
+# from fastapi import APIRouter, Depends, HTTPException
+# from sqlalchemy.orm import Session
+# from config.settings import KAFKA_TRANSACTION_TOPIC
+# from database.db import get_db
+# from models.transaction import Transaction
+# from dto.transaction_create_dto import TransactionCreateDTO
+# from dto.transaction_response_dto import TransactionResponseDTO
+# from dto.transaction_update_dto import TransactionUpdateDTO
+# from fastapi import HTTPException
+# from uuid import UUID
+# from config.kafka_config import get_kafka_producer
+# from config.settings import ENABLE_KAFKA
+# from fastapi import HTTPException
+# from models.account import Account
+
+
+# router = APIRouter(
+#     prefix="/api/transactions",
+#     tags=["Transactions"]
+# )
+
+# # CREATE
+# @router.post("/", response_model=TransactionResponseDTO)
+# def create_transaction(transaction: TransactionCreateDTO, db: Session = Depends(get_db)):
+  
+#     # 🔥 STEP 1: Account Validation (PASTE HERE)
+#     account = db.query(Account).filter(Account.accountId == transaction.fromAccount).first()
+
+#     if not account:
+#         raise HTTPException(status_code=400, detail="Sender account does not exist")
+
+#     # 🔽 NEXT: your existing logic continues
+#     try:
+
+#         new_transaction = Transaction(**transaction.model_dump())
+
+#         print(new_transaction)
+
+#         db.add(new_transaction)
+#         db.commit()
+#         db.refresh(new_transaction)
+
+#         if ENABLE_KAFKA:
+#             kafka = get_kafka_producer()
+#             kafka.send(KAFKA_TRANSACTION_TOPIC,TransactionResponseDTO.model_validate(new_transaction).model_dump(mode="json"))
+
+#         return new_transaction
+        
+#     except Exception as e:
+#         print("ERROR: ",e)
+#         raise HTTPException(
+#             status_code=500,
+#             detail=e.__cause__
+#         )
+
+# # GET ALL
+# @router.get("/", response_model=list[TransactionResponseDTO])
+# def get_all_transactions(db: Session = Depends(get_db)):
+#     return db.query(Transaction).all()
+
+
+# # GET BY ID
+# @router.get("/{transactionId}", response_model=TransactionResponseDTO)
+# def get_transaction(transactionId: UUID, db: Session = Depends(get_db)):
+#     transaction = db.query(Transaction).filter(Transaction.transactionId == transactionId).first()
+#     if not transaction:
+#         raise HTTPException(status_code=404, detail="Transaction not found")
+#     return transaction
+
+
+# # PATCH - Partial Update
+# @router.patch("/{transactionId}", response_model=TransactionResponseDTO)
+# def update_transaction(
+#     transactionId: UUID,
+#     updated_data: TransactionUpdateDTO,
+#     db: Session = Depends(get_db)
+# ):
+#     transaction = db.query(Transaction).filter(
+#         Transaction.transactionId == transactionId
+#     ).first()
+
+#     if not transaction:
+#         raise HTTPException(status_code=404, detail="Transaction not found")
+
+#     for key, value in updated_data.model_dump(exclude_unset=True).items():
+#         setattr(transaction, key, value)
+
+#     db.commit()
+#     db.refresh(transaction)
+
+#     return transaction
+
+# # DELETE
+# @router.delete("/{transactionId}")
+# def delete_transaction(transactionId: UUID, db: Session = Depends(get_db)):
+#     transaction = db.query(Transaction).filter(Transaction.transactionId == transactionId).first()
+#     if not transaction:
+#         raise HTTPException(status_code=404, detail="Transaction not found")
+
+#     db.delete(transaction)
+#     db.commit()
+#     return {"message": "Transaction deleted successfully"}
