dbacd1a8c3d149ab0901807b48051d1fba22c4d9
sohamjoshi <sohamjoshichinchwad@gmail.com>
2026-04-03 07:02:57 +0530

    Updated Hi-Small_trans dataset. Added automatic zip file extraction.

--- Simulation/simulation_tran.py
+++ Simulation/simulation_tran.py
@@ -1,11 +1,25 @@
 import os
 import pandas as pd
 import requests
+import zipfile
 from config.settings import SERVER_URL
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 csv_path = os.path.join(BASE_DIR, "datasets", "HI-Small_Trans.csv")
+zip_path = os.path.join(BASE_DIR, "datasets", "HI-Small_Trans.zip")
+
 url = SERVER_URL + "/api/transactions"
+
+if not os.path.exists(csv_path):
+    print("CSV not found. Extracting from ZIP...")
+
+    if os.path.exists(zip_path):
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(os.path.join(BASE_DIR, "datasets"))
+        print("Extraction complete.")
+    else:
+        raise FileNotFoundError("Neither CSV nor ZIP file found!")
 
 df = pd.read_csv(csv_path)
 TO_PROCESS_RECORDS = 10
