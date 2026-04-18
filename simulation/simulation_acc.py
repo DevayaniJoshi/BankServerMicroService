b67f128d700688686c1a4459b2ecb97ee1db8374
sohamjoshi <sohamjoshichinchwad@gmail.com>
2026-04-18 11:05:55 +0530

    Enabled Kafka and updated folder name

--- simulation/simulation_acc.py
+++ simulation/simulation_acc.py
@@ -3,6 +3,20 @@ import pandas as pd
 import requests
 import zipfile
 from config.settings import SERVER_URL
+import sys
+import time
+
+# default value
+TO_PROCESS_RECORDS = 30
+TIME_DELAY = 100 #ms
+
+# override if argument is passed
+if len(sys.argv) > 1:
+    TO_PROCESS_RECORDS = int(sys.argv[1])
+
+if len(sys.argv) > 2:
+    TO_PROCESS_RECORDS = int(sys.argv[1])
+    TIME_DELAY = int(sys.argv[2])
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -22,7 +36,13 @@ if not os.path.exists(csv_path):
         raise FileNotFoundError("Neither CSV nor ZIP file found!")
 
 df = pd.read_csv(csv_path)
-TO_PROCESS_RECORDS = 10
+
+print("\n=== Account Processor Started ===")
+print(f"Records to process : {TO_PROCESS_RECORDS}")
+print(f"Time delay         : {TIME_DELAY} ms")
+print(f"API Endpoint       : {url}")
+print(f"CSV Path           : {csv_path}")
+print("========================================\n")
 
 count = 0 
 for i, row in df.head(TO_PROCESS_RECORDS).iterrows():
@@ -47,6 +67,7 @@ for i, row in df.head(TO_PROCESS_RECORDS).iterrows():
     else:
         print(count)
 
+    time.sleep(TIME_DELAY / 1000)
 
 
 
--- simulation/simulation_tran.py
+++ simulation/simulation_tran.py
@@ -3,12 +3,29 @@ import pandas as pd
 import requests
 import zipfile
 from config.settings import SERVER_URL
+import sys
+import time
+
+# default value
+TO_PROCESS_RECORDS = 30
+TIME_DELAY = 100 #ms
+
+# override if argument is passed
+if len(sys.argv) > 1:
+    TO_PROCESS_RECORDS = int(sys.argv[1])
+
+if len(sys.argv) > 2:
+    TO_PROCESS_RECORDS = int(sys.argv[1])
+    TIME_DELAY = int(sys.argv[2])
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 csv_path = os.path.join(BASE_DIR, "datasets", "HI-Small_Trans.csv")
 zip_path = os.path.join(BASE_DIR, "datasets", "HI-Small_Trans.zip")
 
+# override if argument is passed
+if len(sys.argv) > 1:
+    TO_PROCESS_RECORDS = int(sys.argv[1])
 url = SERVER_URL + "/api/transactions"
 
 if not os.path.exists(csv_path):
@@ -22,7 +39,13 @@ if not os.path.exists(csv_path):
         raise FileNotFoundError("Neither CSV nor ZIP file found!")
 
 df = pd.read_csv(csv_path)
-TO_PROCESS_RECORDS = 10
+
+print("\n=== Transaction Processor Started ===")
+print(f"Records to process : {TO_PROCESS_RECORDS}")
+print(f"Time delay         : {TIME_DELAY} ms")
+print(f"API Endpoint       : {url}")
+print(f"CSV Path           : {csv_path}")
+print("========================================\n")
 
 count = 0 
 for i, row in df.head(TO_PROCESS_RECORDS).iterrows():
@@ -36,8 +59,8 @@ for i, row in df.head(TO_PROCESS_RECORDS).iterrows():
             "receivingCurrency": row["Receiving Currency"],
             "amountPaid": float(row["Amount Paid"]),
             "paymentCurrency": row["Payment Currency"],
-            "paymentFormat": row["Payment Format"]
-            
+            "paymentFormat": row["Payment Format"],
+            "timestamp": row["Timestamp"]
         }
 
         count += 1
@@ -45,8 +68,11 @@ for i, row in df.head(TO_PROCESS_RECORDS).iterrows():
     
         if response.status_code >= 300:
             print(f"ERROR: Count: {count} \nResponse Status: {response.status_code} \nResponse Body: {response.json()}")
+            
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     else:
         print(count)
+
+    time.sleep(TIME_DELAY / 1000)
